566f9c2d23252214811aabe306694c2fc12e165d
Federica Santoro <federica.santoro.1993@gmail.com>
2021-11-10 17:42:03 +0100

    esercizio non svolto

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function sendCredentials(username) {
+    // console.log(h1.textContent.length);       17
+    const str = h1.textContent;                //Buongiorno [NOME]
+    const nameTC = str.slice(11, 17);         //[NOME]
+    //console.log(str.slice(11, 17));          // [NOME]
+    //console.log(nameTC.length);               6
+    //console.log(nameTC);                      //[NOME]
+    //console.log(h1.textContent.slice(11, 17));  //[NOME]
+    h1.textContent.replace('nameTC','username');
+
+    //HO CREATO UN MOSTRO... NON FUNZIONANTE
+    
+    
     //userPage.firstElementChild.textContent = `Buongiorno ${username}`;
-    userPage.firstElementChild.textContent.replace('[NOME]', 'username');
+    // console.log(h1.textContent); Buongiorno [NOME]
+    
+
+    //userPage.firstElementChild.textContent.replace('[NOME]', 'username');
     // console.log(userPage.firstElementChild.textContent);
     // userPage.firstElementChild.textContent.replace("NOME", username);
   }
@@ -19,7 +34,8 @@ function sendCredentials(username) {
   const userInputEl = document.querySelector("#user");
   const passInputEl = document.querySelector("#pass");
   const btnSendEl = document.querySelector("#btnSend");
-  
+  const h1 = document.querySelector("#goodmorning");
+
   const user = {
     username: "",
     password: "",
@@ -39,3 +55,5 @@ function sendCredentials(username) {
   
     sendCredentials(userStorage);
   });
+
+  
